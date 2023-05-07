3acb987b3e4fa9b69403d1ebf682a9ab826fff26
askerli <askerlinahid09@gmail.com>
2023-05-07 01:08:03 -0700

    first commit

---
+++ src/main/java/com/example/team007/Team007Application.java
@@ -0,0 +1,13 @@
+package com.example.team007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team007Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team007Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team007/Team007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team007ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
